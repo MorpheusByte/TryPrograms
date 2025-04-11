325909bebb2628bb5f27c53defed09f07945db24
MorpheusByte <tahasaidcakmak@icloud.com>
2025-04-11 20:24:16 +0300

    JsStarted

---
+++ Wallet2-App/app.js
@@ -0,0 +1,26 @@
+//?SELECTORS
+//HARCAMA FORMU
+
+const harcamaFormu=document.getElementById("harcama-formu")
+const tarihInput=document.getElementById("tarih")
+const miktarInput=document.getElementById("miktar")
+const harcamaAlaniInput=document.getElementById("harcama-alani")
+
+//HARCAMA TABLOSU
+const harcamaBody=document.getElementById("harcama-body")
+
+// EKLE FORMU
+const ekleBtn = document.getElementById("ekle-btn");
+
+const gelirInput = document.getElementById("gelir-input");
+const ekleFormu = document.getElementById("ekle-formu");
+
+
+// SONUÇ TABLOSU
+const gelirinizTable = document.getElementById("geliriniz");
+
+
+const giderinizTable = document.getElementById("gideriniz");
+
+
+const kalanTable = document.getElementById("kalan");
