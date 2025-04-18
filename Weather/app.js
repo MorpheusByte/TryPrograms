58fa4f066a9a9cb76747b8cc3019c536b11b5462
MorpheusByte <tahasaidcakmak@icloud.com>
2025-04-18 20:54:09 +0300

    konsol ile seçildi

---
+++ Weather/app.js
@@ -0,0 +1,23 @@
+//! Selectors
+const form = document.querySelector("form");
+const input = document.querySelector("form input");
+
+// console.log(input);
+
+//! Variable
+const apiKey = '47d2bf9cf0fc638765792e3f9d510309'
+let url;
+let units = 'metric' 
+// fahrenayt için imperial yazmalıyız
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    // console.log(input.value); kullanıcının girdiği veri
+
+    const city = input.value
+
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&${units}`
+
+    console.log(url);
+    form.reset()
+})
