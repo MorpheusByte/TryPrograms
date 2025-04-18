af0ca4e404e706c57d4bec7d6d9c6f590aa11be3
MorpheusByte <tahasaidcakmak@icloud.com>
2025-04-18 21:37:09 +0300

    1

--- Weather/app.js
+++ Weather/app.js
@@ -1,11 +1,16 @@
 //! Selectors
 const form = document.querySelector("form");
 const input = document.querySelector("form input");
+const cardContainer = document.getElementById("card-container")
 
 // console.log(input);
 
 //! Variable
-const apiKey = '47d2bf9cf0fc638765792e3f9d510309'
+// const apiKey = '47d2bf9cf0fc638765792e3f9d510309'
+// localStorage.setItem("apiKey", '47d2bf9cf0fc638765792e3f9d510309')
+
+const apiKey = localStorage.getItem("apiKey")
+
 let url;
 let units = 'metric' 
 // fahrenayt için imperial yazmalıyız
@@ -19,6 +24,8 @@ form.addEventListener("submit", (e)=>{
     url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
 
     // console.log(url);
+
+    getWeatherData()
     form.reset()
 })
 
@@ -27,7 +34,12 @@ form.addEventListener("submit", (e)=>{
 
 const getWeatherData = async () => {
     try {
-        
+        const response = await fetch(url).then((response)=> response.json())
+        console.log(response);
+
+        //? Data destructure
+
+        const {main, name, weather[0].description ,sys} = response
     } catch (error) {
         
     }
